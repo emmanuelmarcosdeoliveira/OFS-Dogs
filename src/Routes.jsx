a7ce603b9f3b95078570a8be9cb3d93a8701a0ce
Emmanuel Marcos de Oliveiera <emmanuelmarcosdeoliveira@gmail.com>
2024-09-21 00:16:36 -0300

    chore: routes have been updated with a private route

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./components/Helper/ProtectedRoute";
 import LoginPasswordLost from "./components/LoginPasswordLost";
 import LoginPasswordResest from "./components/LoginPasswordReset";
+import User from "./components/User";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import LoginCreate from "./pages/LoginCreate";
@@ -9,10 +11,18 @@ const Mainroutes = () => {
     <>
       <Routes>
         <Route path="/" end element={<Home />} />
-        <Route path="/login/*" element={<Login />} />
+        <Route path="login/*" element={<Login />} />
         <Route path="/login/criar" element={<LoginCreate />} />
         <Route path="/login/perdeu" element={<LoginPasswordLost />} />
         <Route path="resetar" element={<LoginPasswordResest />} />
+        <Route
+          path="/conta/*"
+          element={
+            <ProtectedRoute>
+              <User />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </>
   );
