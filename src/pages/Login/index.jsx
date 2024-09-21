331ada9e40a9fc49067a513f7bd4282f4ee511fb
Emmanuel Marcos de Oliveiera <emmanuelmarcosdeoliveira@gmail.com>
2024-09-21 00:27:42 -0300

    styles: initial sterilization of the login page has been completed

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,19 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
 import LoginForm from "../../components/LoginForm";
+import { UserContext } from "../../context/UseContext";
+import styles from "./Login.module.css";
 
 const Login = () => {
-  return <LoginForm />;
+  const { login } = React.useContext(UserContext);
+  if (login === true) return <Navigate to="/conta" />;
+  return (
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <LoginForm />
+      </div>
+    </section>
+  );
 };
 
 export default Login;
