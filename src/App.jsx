5241327983db46252debb073cb3ea907c2220a1b
Emmanuel Marcos de Oliveiera <emmanuelmarcosdeoliveira@gmail.com>
2024-10-31 14:08:54 -0300

    chore:  created the Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import { UserStorage } from "./context/UseContext";
-import Mainroutes from "./Routes";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
 import "./styles/App.css";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <UserStorage>
-          <Header />
-          <Mainroutes />
-          <Footer />
-        </UserStorage>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login/*" element={<Login />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
