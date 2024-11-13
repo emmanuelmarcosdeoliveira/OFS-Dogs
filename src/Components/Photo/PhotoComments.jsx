9d99613de4a1348fc881f02d1fd1a4af15db27a3
Emmanuel Marcos de Oliveiera <emmanuelmarcosdeoliveira@gmail.com>
2024-11-13 11:24:52 -0300

    feat:  developed the functionality of posting comments

--- src/Components/Photo/PhotoComments.jsx
+++ src/Components/Photo/PhotoComments.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { UserContext } from "../../UserContext";
+import PhotoCommentsForm from "./PhotoCommentsForm";
+import styles from "./PhotoComments.module.css";
 
-const PhotoComments = () => {
-  return <div></div>;
+const PhotoComments = (props) => {
+  const [comments, setComments] = React.useState(() => props.comments);
+  const commentSection = React.useRef(null);
+  const { login } = React.useContext(UserContext);
+
+  React.useEffect(() => {
+    commentSection.current.scrollTop = commentSection.current.scrollHeight;
+  }, [comments]);
+
+  return (
+    <>
+      <ul ref={commentSection} className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <strong>{comment.comment_author}: </strong>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
+  );
 };
 
 export default PhotoComments;
