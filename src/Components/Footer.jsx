367f78a23a6cc198b849d11ab1244cf622164863
Emmanuel Marcos de Oliveiera <emmanuelmarcosdeoliveira@gmail.com>
2024-12-09 17:37:05 -0300

    chore: developed the footer component of the application

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
-import styles from './Footer.module.css';
-
+import Dogs from "../Assets/dogs-footer.svg?react";
+import styles from "./Footer.module.css";
 const Footer = () => {
-  return <div className={styles.footer}></div>;
+  return (
+    <footer className={styles.footer}>
+      <Dogs />
+      <p>
+        {" "}
+        &copy; Dogs alguns direitos reservados{" "}
+        <a target="_blannk" href="https://www.origamid.com">
+          Origamid
+        </a>{" "}
+      </p>
+      <p>
+        developed by 🤎{" "}
+        <a target="_blank" href="https://www.ofs.dev.br">
+          Emmanuel Oliveira
+        </a>
+        <span> | OFS</span>
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
